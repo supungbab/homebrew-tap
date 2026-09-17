2f686d484c00109378f5c0916cc1a0fd05e72e3f
Kyeong Myeong Ho <supungbab@gmail.com>
2026-09-17 14:54:35 +0900

    offiky 1.4.0

--- Casks/offiky.rb
+++ Casks/offiky.rb
@@ -1,6 +1,6 @@
 cask "offiky" do
-  version "1.3.1"
-  sha256 "648b239983c3382f987f6e5b830b76161d527c0a4aa0cee629c0e5dd96120aa3"
+  version "1.4.0"
+  sha256 "34d3c670050e35137d79790177257df0e8ac449daf9a0596aff19d0d993c72dc"
 
   url "https://github.com/supungbab/offiky/releases/download/v#{version}/Offiky.zip"
   name "Offiky"
